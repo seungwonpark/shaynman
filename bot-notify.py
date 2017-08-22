21d11e686a183a0d5f30b280745cafe203a5b060
Seungwon Park <psw14041@gmail.com>
2017-08-22 22:54:42 +0900

    bot-notify.py - markdown styling

--- bot-notify.py
+++ bot-notify.py
@@ -4,7 +4,7 @@ with open('token.txt', 'r') as f:
 	bot_token = f.read().replace('\n','')
 
 print('Enter a text message to send:')
-text = input()
+text = '*봇 관련 공지사항*\n' + input()
 
 print('Are you sure to send: %s to %d users? [y/n]' % (text, len(os.listdir('data/user'))))
 yesno = input()
@@ -15,6 +15,6 @@ if(yesno == 'y'):
 			continue
 		userNo = int(x[:-4])
 		print(userNo)
-		requests.get('https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s' % (bot_token, userNo, text))
+		requests.get('https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s&parse_mode=Markdown' % (bot_token, userNo, text))
 else:
 	print('Cancelled.')
